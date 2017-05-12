c3e07191dcb45d4167d13bd29fdff854073efa3b
Orvin Welchez <owelchez@gmail.com>
2017-05-12 11:30:34 -0400

    Dropdown button

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,33 @@ $('#clubLogo').on('click', function(){
 	hidePanelAnswer();
 })*/
 
+function buttonStyleHide(idString) {
+    let button = document.getElementById('\'' + idString + '\'');
+    console.log(button);
+    	if (button.style.display !== "none") {
+        	button.style.display = "none";
+    	} 
+    	else {
+        	button.style.display = "block";
+    	}
+}
+
+
+/*function buttonStyleHide() {
+    let button = document.getElementById("topLinks");
+    console.log(button);
+    	if (button.style.display !== "none") {
+        	button.style.display = "none";
+    	} 
+    	else {
+        	button.style.display = "block";
+    	}
+}*/
+
+
+// buttonStyleHide();
+buttonStyleHide("titleHeader");
+
+var intViewportWidth = window.innerWidth;
+
+console.log(intViewportWidth);
