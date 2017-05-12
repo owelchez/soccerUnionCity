a9cd8c06e7afe2b06e113d642a686874d54dc677
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2017-05-12 19:35:01 -0400

    Dropdown

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,8 +8,18 @@ $('#clubLogo').on('click', function(){
 	hidePanelAnswer();
 })*/
 
-function buttonStyleHide(idString) {
-    let button = document.getElementById('\'' + idString + '\'');
+function hideDropdown() {
+    let dropMenu = document.getElementById("dropMenu");
+    if (dropMenu.style.display !== "none") {
+            dropMenu.style.display = "none";
+        } 
+        else {
+            dropMenu.style.display = "block";
+        }
+}
+
+function buttonStyleHide() {
+    let button = document.getElementById("topLinks");
     console.log(button);
     	if (button.style.display !== "none") {
         	button.style.display = "none";
@@ -20,21 +30,24 @@ function buttonStyleHide(idString) {
 }
 
 
-/*function buttonStyleHide() {
-    let button = document.getElementById("topLinks");
-    console.log(button);
-    	if (button.style.display !== "none") {
-        	button.style.display = "none";
-    	} 
-    	else {
-        	button.style.display = "block";
-    	}
+
+
+
+
+/*if(window.attachEvent) {
+    window.attachEvent('onresize', function() {
+        var windowWidth = window.innerWidth;
+        alert(windowWidth);
+    });
+}
+else if(window.addEventListener) {
+    window.addEventListener('resize', function() {
+        var windowWidth = window.innerWidth;
+        if(windowWidth < 800) {
+            buttonStyleHide();
+        }
+    }, true);
+}
+else {
+    //The browser does not support Javascript event binding
 }*/
-
-
-// buttonStyleHide();
-buttonStyleHide("titleHeader");
-
-var intViewportWidth = window.innerWidth;
-
-console.log(intViewportWidth);
